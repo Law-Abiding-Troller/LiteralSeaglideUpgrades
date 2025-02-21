dea75598520b2197ee76e828d6c6eba5a7653c9f
Law Abiding Troller <colton.trash5.5@proton.me>
2025-02-20 18:04:19 -0600

    Delete SeaglideModConcept/obj directory

--- SeaglideModConcept/obj/Debug/net472/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace SeaglideModConcept
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "SeaglideModConcept";
-        public const string PLUGIN_NAME = "SeaglideModConcept";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
